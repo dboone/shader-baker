116ebb9b1a7e07618ef5991c8ee359f6ccbba958
drbassett <dale.r.bassett@gmail.com>
2015-10-06 22:04:52 -0400

    Attach context menu to program tree view
    Add 'Add Program' option to context menu.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,14 +2,13 @@
 
 namespace ShaderBaker
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
     {
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
+        InitializeComponent();
     }
+}
+
 }
