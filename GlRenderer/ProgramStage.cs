72a619e5ae14feca79c30c9a1d9bb3f79d149d50
Derek Boone <boonesoftware@gmail.com>
2015-11-01 18:22:32 -0500

    Update `ProgramStage` support zer0 enum value

--- GlRenderer/ProgramStage.cs
+++ GlRenderer/ProgramStage.cs
@@ -1,20 +1,32 @@
-﻿using SharpGL;
+﻿using System;
+using SharpGL;
 
 namespace ShaderBaker.GlRenderer
 {
 
-public enum ProgramStage : uint
+public enum ProgramStage
 {
-    Vertex = OpenGL.GL_VERTEX_SHADER,
-    Geometry = OpenGL.GL_GEOMETRY_SHADER,
-    Fragment = OpenGL.GL_FRAGMENT_SHADER
+    Vertex,
+    Geometry,
+    Fragment
 }
 
 public static class ProgramPipelineStageMethods
 {
     public static uint GlEnumValue(this ProgramStage stage)
     {
-        return (uint) stage;
+        switch (stage)
+        {
+        case ProgramStage.Vertex:
+            return OpenGL.GL_VERTEX_SHADER;
+        case ProgramStage.Geometry:
+            return OpenGL.GL_GEOMETRY_SHADER;
+        case ProgramStage.Fragment:
+            return OpenGL.GL_FRAGMENT_SHADER;
+        default:
+            throw new NotImplementedException(
+                "Invalid shader stage: " + stage);
+        }
     }
 }
 
